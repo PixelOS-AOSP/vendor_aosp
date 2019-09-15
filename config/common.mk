11eed8fe19952c78ea42d71459013ef0991cd9fa
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-15 16:03:53 +0530

    vendor: fonts: Add Google Sans font and change output dir
    Change-Id: I7fd353cb2991caa1cb9b7b5dfed86225db44ef86
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -72,6 +72,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
+# Include font files
+include vendor/aosp/config/fonts.mk
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
