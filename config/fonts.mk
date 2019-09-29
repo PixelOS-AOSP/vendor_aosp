7c083f0b74753c91cd441da05e2d9763a37cb818
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-09-29 17:46:51 +0530

    vendor: fonts: Refactor fonts.mk to include all fonts
    Its way too tedious to type out each fonts' path to copy especially since 10 introduced so many styles
    
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Change-Id: I1c84ca37fea03ea7c832a003512701d0c48d10f8
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,6 +1,6 @@
 # Fonts
+LOCAL_PATH := vendor/aosp/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
 PRODUCT_COPY_FILES += \
-    vendor/aosp/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    vendor/aosp/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/aosp/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
-    vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
