e517c25851f74fa589fb951de3a4e2ae8849d18e
Dyneteve <dyneteve@pixelexperience.org>
2020-04-19 17:11:20 +0200

    [SQUASH] vendor: Carrier Settings Updates

--- config/pixel_telephony.mk
+++ config/pixel_telephony.mk
@@ -29,104 +29,253 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     default.pb
 
-# Australia
+# Carriers Databases
 PRODUCT_PACKAGES += \
-    optus_au.pb \
-    telstra_au.pb \
-    vodafone_au.pb
-
-# Canada
-PRODUCT_PACKAGES += \
-    bell_ca.pb \
-    fido_ca.pb \
-    fizz_ca.pb \
-    freedommobile_ca.pb \
-    koodo_ca.pb \
-    luckymobile_ca.pb \
-    pcmobilebell_ca.pb \
-    rogers_ca.pb \
-    solomobile_ca.pb \
-    telus_ca.pb \
-    videotron_ca.pb \
-    virgin_ca.pb
-
-# France
-PRODUCT_PACKAGES += \
-    bouygues_fr.pb \
-    orange_fr.pb \
-    sfr_fr.pb
-
-# Germany
-PRODUCT_PACKAGES += \
-    o2_de.pb \
-    telekom_de.pb \
-    vodafone_de.pb
-
-# India
-PRODUCT_PACKAGES += \
+    1and1_de.pb \
+    2degrees_nz.pb \
+    a1_at.pb \
+    a1_si.pb \
+    ahamo_jp.pb \
     airtel_in.pb \
-    idea_in.pb \
-    rjio_in.pb \
-    vodafone_in.pb
-
-# Italy
-PRODUCT_PACKAGES += \
-    vodafone_it.pb
-
-# Japan
-PRODUCT_PACKAGES += \
-    docomo_jp.pb \
-    kddi_jp.pb \
-    rakuten_jp.pb \
-    softbank_jp.pb
-
-# Netherlands
-PRODUCT_PACKAGES += \
-    vodafone_nl.pb
-
-# Singapore
-PRODUCT_PACKAGES += \
-    singtel_sg.pb \
-    starhub_sg.pb
-
-# Spain
-PRODUCT_PACKAGES += \
-    orange_es.pb \
-    vodafone_es.pb
-
-# Taiwan
-PRODUCT_PACKAGES += \
-    cht_tw.pb \
-    fet_tw.pb \
-    twm_tw.pb
-
-# United Kingdom
-PRODUCT_PACKAGES += \
-    ee_gb.pb \
-    h3_gb.pb \
-    idmobile_gb.pb \
-    o2postpaid_gb.pb \
-    o2prepaid_gb.pb \
-    vodafone_gb.pb
-
-# United States
-PRODUCT_PACKAGES += \
+    ais_th.pb \
+    altice_us.pb \
+    apt_tw.pb \
+    assurance_us.pb \
+    astca_as.pb \
     att5g_us.pb \
+    att5gsa_us.pb \
     att_us.pb \
+    attmvnos_us.pb \
+    base_be.pb \
+    bell_ca.pb \
+    bluegrass_us.pb \
+    boost_us.pb \
+    boosttmo_us.pb \
+    bouygues_fr.pb \
+    btb_gb.pb \
+    btc_gb.pb \
+    carolinawest_us.pb \
+    celcom_my.pb \
     cellcom_us.pb \
+    cellcommno_us.pb \
+    celluarone_us.pb \
+    chatr_ca.pb \
+    chinamobile_hk.pb \
+    cht_tw.pb \
+    cityfone_ca.pb \
+    claro_pr.pb \
+    congstar_de.pb \
+    consumercellularatt_us.pb \
+    consumercellulartmo_us.pb \
+    coppervalley_us.pb \
+    coriolis_fr.pb \
     cricket5g_us.pb \
     cricket_us.pb \
+    cspire_nl.pb \
     cspire_us.pb \
-    firstnetpacific_us.pb \
-    firstnet_us.pb \
+    digi_hu.pb \
+    digi_my.pb \
+    digi_ro.pb \
+    dishatt_us.pb \
+    dito_ph.pb \
+    dna_fi.pb \
+    docomo_jp.pb \
+    docomomvno_jp.pb \
+    du_ae.pb \
+    eastlink_ca.pb \
+    ee_gb.pb \
+    eir_ie.pb \
+    enreach_nl.pb \
+    eplus_de.pb \
+    esn_gb.pb \
+    euskaltel_es.pb \
+    evolve_us.pb \
+    fastweb_it.pb \
+    fet_tw.pb \
+    fi_at.pb \
     fi_us.pb \
+    fido_ca.pb \
+    firstnet_us.pb \
+    firstnetpacific2_us.pb \
+    firstnetpacific_us.pb \
+    fizz_ca.pb \
+    free_fr.pb \
+    freedommobile_ca.pb \
+    gamma_gb.pb \
+    gci_us.pb \
+    gibtel_gi.pb \
+    giga_sg.pb \
+    globe_ph.pb \
+    h3_at.pb \
+    h3_gb.pb \
+    h3_ie.pb \
+    h3_se.pb \
+    h3g27202_ie.pb \
+    halebop_se.pb \
+    homobile_it.pb \
+    ice_no.pb \
+    idea_in.pb \
+    idmobile_gb.pb \
+    iliad_it.pb \
+    jazztel_es.pb \
+    kddi_jp.pb \
+    kddimvno_jp.pb \
+    kena_it.pb \
+    koodo_ca.pb \
+    kpn_nl.pb \
+    liberty_pr.pb \
+    linemo_jp.pb \
+    luckymobile_ca.pb \
+    m1_sg.pb \
+    manxcontract_im.pb \
+    manxprepaid_im.pb \
+    maxis_my.pb \
+    meo_pt.pb \
+    metropcs_us.pb \
+    movistar_es.pb \
+    nextech_us.pb \
+    nos_pt.pb \
+    o2_cz.pb \
+    o2_de.pb \
+    o2_sk.pb \
+    o2postpaid_gb.pb \
+    o2prepaid_de.pb \
+    o2prepaid_gb.pb \
+    onecall_no.pb \
+    ooredoo_qa.pb \
+    optus_au.pb \
+    orange_be.pb \
+    orange_es.pb \
+    orange_fr.pb \
+    orange_gp.pb \
+    orange_lu.pb \
+    orange_pl.pb \
+    orange_re.pb \
+    orange_ro.pb \
+    orange_sk.pb \
+    partner_il.pb \
+    pcmobilebell_ca.pb \
+    pelephone_il.pb \
+    pepephone_es.pb \
+    play_pl.pb \
+    plus_pl.pb \
+    post_lu.pb \
+    povo_jp.pb \
+    proximus_be.pb \
+    publicmobile_ca.pb \
+    rakuten_jp.pb \
+    redpocketatt_us.pb \
+    redpockettmo_us.pb \
+    rjio_in.pb \
+    rogers5g_ca.pb \
+    rogers_ca.pb \
+    salt_ch.pb \
+    sasktel_ca.pb \
+    sfr_fr.pb \
+    shaw_ca.pb \
+    simba_sg.pb \
+    simple_us.pb \
+    singtel_sg.pb \
+    skinny_nz.pb \
+    sky_gb.pb \
+    smartone_hk.pb \
+    smarty_gb.pb \
+    softbank_jp.pb \
+    solomobile_ca.pb \
+    spark_nz.pb \
+    spectrum_us.pb \
     sprint_us.pb \
+    sprintprepaid_us.pb \
     sprintwholesale_us.pb \
+    starhub_sg.pb \
+    stc_sa.pb \
+    strata_us.pb \
+    sunrise_ch.pb \
+    swisscom_ch.pb \
+    swisscom_li.pb \
+    talkmobile_gb.pb \
+    tango_us.pb \
+    tbaytel_ca.pb \
+    tchibo_de.pb \
+    tdc_dk.pb \
+    tele2_se.pb \
+    telekom_de.pb \
+    telekom_gr.pb \
+    telekom_hr.pb \
+    telekom_hu.pb \
+    telekom_pl.pb \
+    telekom_ro.pb \
+    telekom_si.pb \
+    telekom_sk.pb \
+    telemach_si.pb \
+    telenet_be.pb \
+    telenor_dk.pb \
+    telenor_no.pb \
+    telenor_se.pb \
+    telia_dk.pb \
+    telia_ee.pb \
+    telia_fi.pb \
+    telia_lt.pb \
+    telia_no.pb \
+    telia_se.pb \
+    telstra_au.pb \
+    telus_ca.pb \
+    tesco_gb.pb \
+    three_dk.pb \
+    tim_it.pb \
+    tmobile_at.pb \
+    tmobile_cz.pb \
+    tmobile_nl.pb \
     tmobile_us.pb \
+    tmomvno_nl.pb \
+    tmomvno_us.pb \
+    tracfoneatt_us.pb \
+    tracfonetmo_us.pb \
+    tracfoneverizon_us.pb \
+    truemove_th.pb \
+    tstar_tw.pb \
+    twm_tw.pb \
+    u264a_ca.pb \
+    u264a_us.pb \
+    u264ao_at.pb \
+    u264ap_at.pb \
+    ultra_us.pb \
+    umobile_my.pb \
+    unifi_my.pb \
+    uqmobile5gsa_jp.pb \
+    uqmobile_jp.pb \
     uscc_us.pb \
     verizon_us.pb \
+    verymobile_it.pb \
+    viaero_us.pb \
+    videotron_ca.pb \
+    vimla_se.pb \
+    virgin_ca.pb \
+    virgin_gb.pb \
+    virgin_us.pb \
     visible_us.pb \
-    xfinity_us.pb
+    visiblev_us.pb \
+    vodafone_au.pb \
+    vodafone_cz.pb \
+    vodafone_de.pb \
+    vodafone_es.pb \
+    vodafone_gb.pb \
+    vodafone_hu.pb \
+    vodafone_ie.pb \
+    vodafone_in.pb \
+    vodafone_it.pb \
+    vodafone_nl.pb \
+    vodafone_nz.pb \
+    vodafone_pt.pb \
+    vodafone_ro.pb \
+    vodafone_tr.pb \
+    windtre_it.pb \
+    xfinity_us.pb \
+    yes_my.pb \
+    yettel_hu.pb \
+    yoigo_es.pb \
+    zain_kw.pb \
+    zeop_re.pb
 
 # Other countries
 PRODUCT_PACKAGES += \
@@ -135,3 +284,7 @@ PRODUCT_PACKAGES += \
 # Disable mobile data in roaming by default.
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
+
+# Include Carrier Runtime Configuration
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/aosp/CarrierSettings/overlay
