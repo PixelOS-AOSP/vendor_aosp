b46a5d80803f730367da3575c79661302a524156
Jesse Chan <jc@lineageos.org>
2020-05-19 14:01:25 +0800

    Implement hide gestural navigation hint bar [3/3]
    Change-Id: I5ed5c55ec46c3bb5e91aeb88bd5f06bd4445c2f0
    Signed-off-by: Jesse Chan <jc@lineageos.org>

--- config/packages.mk
+++ config/packages.mk
@@ -77,6 +77,10 @@ PRODUCT_COPY_FILES += \
      vendor/aosp/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
      vendor/aosp/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
 
+# Hide navigation bar hint
+PRODUCT_PACKAGES += \
+    NavigationBarNoHintOverlay
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
