2f961b6e8981931bd81ad9aac17588dde689146f
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-11 23:03:27 +0530

    vendor: Import GoogleSans Regular and Italic and all its Customizations
    Change-Id: I3455fe02b2ed309c4bf586b576222373a787f3d7

--- config/packages.mk
+++ config/packages.mk
@@ -64,6 +64,12 @@ PRODUCT_PACKAGES += \
     mkfs.ntfs \
     mount.ntfs
 
+# Fonts
+PRODUCT_COPY_FILES += \
+     vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+     vendor/aosp/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+     vendor/aosp/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
+
 # Openssh
 PRODUCT_PACKAGES += \
     scp \
