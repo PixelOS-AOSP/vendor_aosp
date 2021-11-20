98ae3a6acf707cef9dd64256d69ca940991e8943
AdarshGrewal <adarshgrewal@gmail.com>
2021-11-20 18:36:59 +0530

    vendor: gms: Import ime props
    * taken from Pixel 6 Pro
    
    Change-Id: I803f27ca3a638e459e1952399f7234181c733e53

--- config/gms.mk
+++ config/gms.mk
@@ -5,3 +5,9 @@
 
 # Google Apps
 $(call inherit-product, vendor/gms/products/gms.mk)
+
+# Gboard
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
