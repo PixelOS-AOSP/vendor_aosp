fa7d4d3ea3bd6f55cb83a99b3dc25b9fc6acddd6
darkbeast69 <guptaaryan189@gmail.com>
2021-12-23 15:28:26 +0530

    config: Include Our Custom Overlays

--- config/common.mk
+++ config/common.mk
@@ -210,4 +210,7 @@ $(call inherit-product, vendor/aosp/config/ota.mk)
 # RRO Overlays
 $(call inherit-product, vendor/aosp/config/rro_overlays.mk)
 
+# Custom Overlays
+$(call inherit-product, vendor/overlays/overlays.mk)
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
