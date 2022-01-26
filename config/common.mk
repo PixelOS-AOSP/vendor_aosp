27f72f63e035eb0c7654297f9c3c1066523be43b
Danny Lin <danny@kdrag0n.dev>
2022-01-26 21:18:17 +0530

    common: Build Repainter integration service (RepainterServicePriv)
    https: //github.com/kdrag0n/android_packages_apps_RepainterService
    Change-Id: I2ad2d06211af62c1b254425552db28e3f3906988

--- config/common.mk
+++ config/common.mk
@@ -136,6 +136,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
 
+# Repainter integration
+PRODUCT_PACKAGES += \
+    RepainterServicePriv
+
 # Extra tools
 PRODUCT_PACKAGES += \
     bash \
