b28b2e9227eb4aedd76641e8fd35a73238d98865
Karan Parashar <whyredfire@gmail.com>
2022-06-16 23:29:02 +0530

    vendor: Import monet bootanimation (720p)
    Change-Id: I8b19f1be00f68a06feb77a77521aa86d6ad97cf7

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,7 +1,10 @@
 # Boot Animation
 
+# 720p
+ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # 1080p
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
 PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # 1440p
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
