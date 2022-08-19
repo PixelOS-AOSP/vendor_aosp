aca9cb297494a66c23f24bc43aeb0d041b39c22b
Karan Parashar <whyredfire@gmail.com>
2022-08-19 18:10:59 +0200

    vendor: config: Dex preopt PixelLauncher
    Change-Id: I2f6690fed89b48f05f8059d23a1450bc5e5631a1

--- config/common.mk
+++ config/common.mk
@@ -24,7 +24,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
-    Launcher3QuickStep
+    NexusLauncherRelease
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
