420cdb9489a039759ae880a3db782b53e11dccd8
Karan Parashar <whyredfire@gmail.com>
2022-08-20 04:13:15 +0200

    vendor: bootanimation : Set to 1080p if not defined
    Change-Id: I27636fb65c93d3740509b3da4a02a0a013a5cdd8

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -9,7 +9,7 @@ PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/720.zip:$(TARGET
 # 480p
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
 PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/480.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-# Default to 720p if the device does not set the flag.
+# Default to 1080p if the device does not set the flag.
 else
-PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
