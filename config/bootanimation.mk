c8b2efd56406b3a90b626a2fb134696e0f27305a
Karan Parashar <whyredfire@gmail.com>
2022-08-20 04:34:14 +0200

    vendor: bootanimation: Import from Pixel
    Change-Id: I6951a103b5ad4c0ba86b1989bd0ae4c121e580e6

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -3,6 +3,9 @@
 # 1080p
 ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
 PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+# 1440p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # Default to 1080p if the device does not set the flag.
 else
 PRODUCT_COPY_FILES += vendor/aosp/prebuilt/common/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
