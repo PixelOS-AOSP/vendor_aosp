8e31d107e43ee6f077968a1033c9030297f61b4d
basamaryan <basam.aryan@gmail.com>
2022-08-28 12:00:07 +0200

    overlay: Import themed icons from arrow-13.1
    commit cf23180aeaec376a6e9943c72b7aae10e463caf8
    Author: Karan Parashar <whyredfire@gmail.com>
    Date:   Fri May 24 02:02:39 2024 +0530
    
        ThemedIconsOverlay: Update anxcamera icon
    
         from https://github.com/AOSPA/android_vendor_aospa/blob/uvite/overlay/static/packages/apps/Launcher3/res/drawable/themed_icon_miuicamera.xml
    
    commit f8b4a7583e9d0c9f5b7487fe21554d0c07680582
    Author: Aditya Singh <adityasingh1768@gmail.com>
    Date:   Sat Dec 16 16:35:28 2023 +0530
    
        overlay: Use camera themed icon for Sony Camera and Photo Pro (#3)
    
        Change-Id: Ibd09171cb50096fec69178b7d30f08fbb94326b1
        Signed-off-by: Aditya Singh <adityasingh1768@gmail.com>
    
    commit f3466100e5e1a1a112835d9268c94a77d1165815
    Author: Karan Parashar <karan@pixelos.net>
    Date:   Tue Jul 18 00:02:34 2023 +0200
    
        overlay: Use ANX Camera icon for Oplus Camera
    
        Change-Id: Iacdc6fbf36f090f893c10df62c88f7c135982937
    
    commit 6d9b8fdb1991b2f9832a4ca6b374f17111f1a752
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Sun Apr 9 06:52:21 2023 +0530
    
        overlay: Theme motorola camera and dolby packages
    
        Moto cam themed icon by @ yukiscape
    
        Change-Id: I88006a9cb1b769fb817da44ed75246f9d36f943b
        Co-authored-by: amartya <garikapatiamartya@gmail.com>
    
    commit 17580e95a484777d9693858d1abe8b84ba8ad247
    Author: Sonal Singh <sonal.singh.19993@gmail.com>
    Date:   Mon Dec 26 10:43:52 2022 +0530
    
        ArrowLauncher: Update some google apps themed icons
    
         - Google Dialer, Google Messages, Google Contacts and Google Duo(Now Meet)
           icons have been updated by Google but the themed icons still using the
           old icon design until now
    
    commit 8c527dbfba86aed1a8f754e594db8cda05c93214
    Author: Sonal Singh <sonal.singh.19993@gmail.com>
    Date:   Mon Dec 26 09:12:19 2022 +0530
    
        ArrowLauncher: Update amazaon shopping, prime video and magisk themed icons
    
         - better and bolder icons
    
    commit 2036484c774292e679efb9fed53c268fe26bd831
    Author: Sonal Singh <sonal.singh.19993@gmail.com>
    Date:   Mon Dec 26 19:15:55 2022 +0530
    
        ArrowLauncher: Map OLX Southasia package name to olx themed icon
    
    commit d5b90d668a1848b9e82df60020108d3d13ef927d
    Author: Sonal Singh <sonal.singh.19993@gmail.com>
    Date:   Mon Dec 26 18:50:48 2022 +0530
    
        ArrowLauncher: Map LMC 8.4 and GCamGO camera to camera icon
    
         - GCamGO now uses OG GoogleCamera icon with Camera as app label
         - also map olx southasia package name olx app tonolx thmed icon
    
    commit 9be2afa2ac1cb1611e26f1534513ede7a70cc115
    Author: Arber <88089607+ArberTu474@users.noreply.github.com>
    Date:   Fri Oct 21 21:16:27 2022 +0200
    
        ArrowLauncher: ThemedIcons: Add Desmos icon
    
    commit e057be685d5f0fb7b6c7266ff0599224752fd0bd
    Author: Arber <88089607+ArberTu474@users.noreply.github.com>
    Date:   Sat Oct 15 03:30:07 2022 +0200
    
        ArrowLauncher: Added themed icons for Profili im, NokoPrint, TouchVPN
    
    commit 7b1041c52a69f4d864f9bef989c33e8b1246ffa1
    Author: mukesh22584 <mks22584@gmail.com>
    Date:   Wed Dec 14 10:04:40 2022 +0000
    
        ArrowLauncher: Theme OnePlus camera
    
    commit 38c180b924e6f6f5852145c289ecd35630976229
    Author: PainKiller3 <ninadpatil100@gmail.com>
    Date:   Wed Dec 21 11:01:15 2022 +0000
    
        ArrowLauncher: Update themed icons
    
        * From https://t.me/modulesrepo
    
        Change-Id: I7157b02821d694d580fe55578362cecb739d57ba
    
    commit 1553ff2798cde3ea375c7503ca46c7845365673f
    Author: PainKiller3 <ninadpatil100@gmail.com>
    Date:   Thu Oct 6 09:04:15 2022 +0000
    
        ArrowLauncher: Update themed icons
    
        * From https://t.me/modulesrepo
    
        Change-Id: Ic1610b33286868ecfe2e7c0d38eded9477a6b243
    
    Change-Id: Ie0224dac4d8374271ab5483c016c7f2989e1c9c3

--- config/common.mk
+++ config/common.mk
@@ -222,7 +222,8 @@ PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
-    NavigationBarNoHintOverlay
+    NavigationBarNoHintOverlay \
+    ThemedIconsOverlay
 
 # Translations
 CUSTOM_LOCALES += \
