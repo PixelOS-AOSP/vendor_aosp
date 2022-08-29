e9f1834cb869d54a5af95f32fdf891dd9c5367e9
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 02:39:47 +0300

    config: Remove TARGET_USES_QCOM_BSP_LEGACY
    Change-Id: I93f111285c46e1a1f811b9d8555de96474d60386

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -49,7 +49,6 @@ SOONG_CONFIG_NAMESPACES += customQcomVars
 SOONG_CONFIG_customQcomVars += \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd \
-    uses_qcom_bsp_legacy \
     uses_qti_camera_device \
     needs_camera_boottime_timestamp
 
@@ -67,7 +66,6 @@ SOONG_CONFIG_customGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_N
 SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_customQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
-SOONG_CONFIG_customQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
 SOONG_CONFIG_customQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 SOONG_CONFIG_customQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
