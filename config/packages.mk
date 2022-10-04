fd467f157f60cc4b415b79c43e23c7409d444b0e
Nolen Johnson <johnsonnolen@gmail.com>
2022-10-03 21:16:56 -0400

    vendor: config: Exclude ntfs-3g from artifact path requirement
    Change-Id: I6253ac0b326f86ba7fd78f6babb9ee2242ba9133

--- config/packages.mk
+++ config/packages.mk
@@ -64,6 +64,13 @@ PRODUCT_PACKAGES += \
     mkfs.ntfs \
     mount.ntfs
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
 # Fonts
 PRODUCT_COPY_FILES += \
      vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
