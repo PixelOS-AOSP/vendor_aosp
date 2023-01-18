303127a6d22269ee0bba6f73abc4ab609e3cad3b
Karan Parashar <whyredfire@gmail.com>
2023-01-19 03:53:27 +0530

    common: Conditionally build Updater on official builds
    Change-Id: I821e377c36e1426134e1c68fb23824afb3402e43

--- config/common.mk
+++ config/common.mk
@@ -85,13 +85,6 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_PACKAGES += \
     build-manifest
 
-# PixelOS packages
-PRODUCT_PACKAGES += \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc \
-
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
@@ -192,5 +185,8 @@ CUSTOM_LOCALES += \
     fur_IT
 
 include vendor/aosp/config/version.mk
+
+# OTA
+$(call inherit-product, vendor/aosp/config/ota.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
---
+++ config/ota.mk
@@ -0,0 +1,8 @@
+# Updater
+ifeq ($(IS_OFFICIAL),true)
+    PRODUCT_PACKAGES += \
+        Updater
+
+    PRODUCT_COPY_FILES += \
+        vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
+endif
