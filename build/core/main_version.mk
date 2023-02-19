03402859e6c13f9eeed7d119f443ee35663a466f
Karan Parashar <karan@pixelos.net>
2023-02-19 20:47:01 +0000

    vendor: ota: Inline with our updater
    [1]: PixelOS-AOSP/packages_apps_Updates@10d53fd86d36b7299bd01aa22e210f886f46cc78
    [2]: PixelOS-AOSP/packages_apps_Settings@2487f236e9ed62c633769152b99ba6139f6eb794
    
    Change-Id: I7aa2b98c11548d544aaa9954946f094ef9c90be6

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -7,13 +7,14 @@ endif
 # PixelOS Platform Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.device=$(CUSTOM_BUILD) \
     ro.custom.fingerprint=$(ROM_FINGERPRINT) \
     ro.custom.version=$(CUSTOM_VERSION) \
-    ro.custom.device=$(CUSTOM_BUILD) \
     ro.modversion=$(CUSTOM_VERSION)
 
 # Updater
 ifeq ($(IS_OFFICIAL),true)
     ADDITIONAL_SYSTEM_PROPERTIES  += \
-        ro.is_official=true
+        net.pixelos.build_type=ci \
+        net.pixelos.version=$(CUSTOM_VERSION_PROP)
 endif
