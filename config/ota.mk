1474de1aceb09d45f3b17b6b5a74a4003b8f9e17
Karan Parashar <karan@pixelos.net>
2023-02-19 20:47:01 +0000

    vendor: ota: Inline with our updater
    [1]: PixelOS-AOSP/packages_apps_Updates@10d53fd86d36b7299bd01aa22e210f886f46cc78
    [2]: PixelOS-AOSP/packages_apps_Settings@2487f236e9ed62c633769152b99ba6139f6eb794
    
    Change-Id: I7aa2b98c11548d544aaa9954946f094ef9c90be6

--- config/ota.mk
+++ config/ota.mk
@@ -1,7 +1,7 @@
 # Updater
 ifeq ($(IS_OFFICIAL),true)
     PRODUCT_PACKAGES += \
-        Updater
+        Updates
 
     PRODUCT_COPY_FILES += \
         vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
