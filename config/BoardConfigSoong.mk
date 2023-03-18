632b3ac604c9d0fcfc520e138eb82ac122bf85ca
Nolen Johnson <johnsonnolen@gmail.com>
2023-03-18 00:31:32 -0400

    soong: Bring bootloader_message_offset back
    This is squashed revert of:
    
        Revert "config: Remove BOOTLOADER_MESSAGE_OFFSET"
    
        This reverts commit a8bbd0e4aa41915818774984e04c977afe234498.
    
        Change-Id: I66df34e901e87e543102377469a150a9bf03e5b8
    
        Revert "Revert "soong: Export bootloader_message_offset to dependencies""
    
        This reverts commit d0b4f9894add43e5caa8720575391e781cab2cf2.
    
        Reason for revert: Bringing this back, LGE 8996 lives.
    
        Change-Id: Iae68fee53ba647d8c64c54388e7296232adf4db8
    
    Change-Id: I358d8e19f2d4da590c9579c1473b5f1964bb71aa

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -29,6 +29,7 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += customGlobalVars
 SOONG_CONFIG_customGlobalVars += \
     additional_gralloc_10_usage_bits \
+    bootloader_message_offset \
     gralloc_handle_has_reserved_size \
     target_init_vendor_lib \
     target_ld_shim_libs \
@@ -58,6 +59,7 @@ SOONG_CONFIG_customQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE
 SOONG_CONFIG_customQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
+BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE ?= false
 TARGET_INIT_VENDOR_LIB ?= vendor_init
@@ -65,6 +67,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_customGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_customGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_customGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_customGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_customGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
