123491f721a11be56bf04e470077ae3f2efebe40
Dyneteve <dyneteve@hentaios.com>
2023-11-17 21:08:42 +0530

    config: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -211,6 +211,13 @@ PRODUCT_PACKAGES += \
     ThemedIconsOverlay \
     NavigationBarNoHintOverlay
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
 # Translations
 CUSTOM_LOCALES += \
     ast_ES \
