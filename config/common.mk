cba30d055a5dffdf57217c5f59ada565a78edd18
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2023-12-11 18:55:29 +0100

    overlay: Move out CertifiedPropsOverlay and partition order definiton
    Change-Id: I1382ccbc9c993bd90b293d9003643335b3c3bbe0

--- config/common.mk
+++ config/common.mk
@@ -98,10 +98,6 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
 
 # Overlay
-PRODUCT_COPY_FILES += \
-    vendor/aosp/overlay/rro_packages/config-system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/overlay/config/config.xml \
-    vendor/aosp/overlay/rro_packages/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
-
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
 
@@ -225,7 +221,6 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay \
-    CertifiedPropsOverlay \
     CustomFontPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
