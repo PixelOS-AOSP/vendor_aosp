43758fa35c65e2237aa9cd3c9de1e20ed68444a4
LuK1337 <priv.luk@gmail.com>
2024-02-17 18:27:57 +0100

    config: Ship EmergencyInfo on WiFi only tablets
    Change-Id: I89e16f716076d5351949c0bb13f55fc4eaeee4f5

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -2,3 +2,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
 # Inherit full common stuff
 $(call inherit-product, vendor/aosp/config/common_full.mk)
+
+$(call inherit-product, vendor/aosp/config/wifionly.mk)
