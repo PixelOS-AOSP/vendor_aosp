1462ab6a07245b2e75b020a6ff75d6c322a51523
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-05-07 21:36:24 -0700

    config: don't build AudioFX,bash,htop,nano,vim (#20)
    These aren't forked here.

--- config/common_full.mk
+++ config/common_full.mk
@@ -8,11 +8,6 @@ PRODUCT_PACKAGES += \
     Aperture
 endif
 
-ifneq ($(TARGET_EXCLUDES_AUDIOFX),true)
-PRODUCT_PACKAGES += \
-    AudioFX
-endif
-
 # Extra cmdline tools
 PRODUCT_PACKAGES += \
     unrar \
