ba6bf65050526b94c0c29d2b786712d4bdd756b8
basamaryan <basam.aryan@gmail.com>
2024-07-25 11:14:19 +0200

    prebuilt: Drop unused sensitive_pn.xml
    Change-Id: I40674d04b7b0ed6f75314e10f74c400912107667

--- config/telephony.mk
+++ config/telephony.mk
@@ -3,10 +3,6 @@ ifeq ($(WITH_GMS),true)
 WITH_GMS_COMMS_SUITE := true
 endif
 
-# Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
-
 # World APN list
 PRODUCT_PACKAGES += \
     apns-conf.xml
