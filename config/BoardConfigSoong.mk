0122777ba27080b729b50633726ef4042d92c883
LuK1337 <priv.luk@gmail.com>
2024-10-30 08:37:45 +0100

    soong: Remove LD_SHIM_LIBS support
    Change-Id: I4a45fdd9a140a95aa42861bf294bfb8e8bb91f34

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -42,7 +42,6 @@ SOONG_CONFIG_customGlobalVars += \
     target_health_charging_control_supports_toggle \
     target_libcameraservice_ext_lib \
     target_init_vendor_lib \
-    target_ld_shim_libs \
     target_power_libperfmgr_mode_extension_lib \
     target_powershare_path \
     target_powershare_enabled \
@@ -126,7 +125,6 @@ SOONG_CONFIG_customGlobalVars_target_health_charging_control_supports_bypass := 
 SOONG_CONFIG_customGlobalVars_target_health_charging_control_supports_deadline := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE)
 SOONG_CONFIG_customGlobalVars_target_health_charging_control_supports_toggle := $(TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE)
 SOONG_CONFIG_customGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
-SOONG_CONFIG_customGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_customGlobalVars_target_libcameraservice_ext_lib := $(TARGET_CAMERA_SERVICE_EXT_LIB)
 SOONG_CONFIG_customGlobalVars_target_power_libperfmgr_mode_extension_lib := $(TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB)
 SOONG_CONFIG_customGlobalVars_target_powershare_path := $(TARGET_POWERSHARE_PATH)
