f6d772c4b5cec79bab866d9b1cd3c5e151b9d5b6
John Galt <johngaltfirstrun@gmail.com>
2025-01-07 15:47:15 -0500

    config/version: add TARGET_NO_OEM_UNLOCK
    Reportedly if this is flipped, Samsung devices brick. So allow them to
    opt out
    
    Change-Id: I0d5fc0e06c1b1646be08258ea4bb27068a145574

--- config/version.mk
+++ config/version.mk
@@ -23,7 +23,9 @@ endif
 ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/aosp/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/aosp/signing/keys/releasekey
+ifneq ($(TARGET_NO_OEM_UNLOCK),true)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
 endif
 ifneq (,$(wildcard vendor/aosp/signing/keys/otakey.x509.pem))
 PRODUCT_OTA_PUBLIC_KEYS := vendor/aosp/signing/keys/otakey.x509.pem
