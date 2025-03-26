8867ff4aeb3caff25a7e75b86bc7b6f51ffa9a77
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 19:01:54 +0200

    soong: move nvidia enhancements config to select
    Original flag is still used by device specific makefile guards.
    
    Change-Id: I528ee2a2a25680994561e4c3fc377f7b57106a89

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -59,17 +59,12 @@ SOONG_CONFIG_customGlobalVars += \
     target_health_charging_control_charging_path
 endif
 
-SOONG_CONFIG_NAMESPACES += customNvidiaVars
-SOONG_CONFIG_customNvidiaVars += \
-    uses_nvidia_enhancements
-
 # Soong bool variables
 SOONG_CONFIG_customGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
 SOONG_CONFIG_customGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_customGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_customGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_customGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
-SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
