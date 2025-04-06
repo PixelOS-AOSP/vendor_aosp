bda494564965e8daa0535ffd054c779c9a70535b
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-04-06 09:08:11 +0545

    config: Determine bootanimation resolution from TARGET_SCREEN_WIDTH
    Change-Id: Id3d4e226a62a6b672580463c7354c2f6e66aa651
    Signed-off-by: Hridaya Prajapati <info.hridayaprajapati@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,13 +1,13 @@
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+ifeq ($(TARGET_SCREEN_WIDTH),1080)
      PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+else ifeq ($(TARGET_SCREEN_WIDTH),1440)
      PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+else ifeq ($(TARGET_SCREEN_WIDTH),720)
      PRODUCT_COPY_FILES += vendor/aosp/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
-    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
-        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    ifeq ($(TARGET_SCREEN_WIDTH),)
+        $(warning "TARGET_SCREEN_WIDTH is undefined, assuming 1080p")
     else
         $(warning "Current bootanimation res is not supported, forcing 1080p")
     endif
