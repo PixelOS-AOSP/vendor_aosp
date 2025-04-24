b67f42b219fce05802dcb55ae82a6bd89ce3d35d
Michael Bestas <mkbestas@lineageos.org>
2025-04-25 00:17:18 +0300

    config: Move additional gralloc 10 usage bits to select()
    Change-Id: If1542af472e74cd953d9ca093dd225dbba028aea

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -26,16 +26,6 @@ endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
-SOONG_CONFIG_NAMESPACES += customGlobalVars
-SOONG_CONFIG_customGlobalVars += \
-    additional_gralloc_10_usage_bits
-
-# Set default values
-TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
-
-# Soong value variables
-SOONG_CONFIG_customGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
-
 # Camera package name
 ifneq ($(TARGET_CAMERA_PACKAGE_NAME),)
     $(call soong_config_set,camera,package_name,$(TARGET_CAMERA_PACKAGE_NAME))
@@ -49,6 +39,11 @@ endif
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)
     $(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
+endif
+
+# Libui
+ifneq ($(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS),)
+    $(call soong_config_set,libui,additional_gralloc_10_usage_bits,$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 endif
 
 # Lineage Health HAL
